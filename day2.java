09d9351aa058e039ec9a3a7bf0b41b3830aeb221
SitiNurRahma <sitinurrahma249@gmail.com>
2023-10-10 16:32:24 +0800

    day2

---
+++ day2.java
@@ -0,0 +1,10 @@
+
+package ISC;
+
+public class day2 {
+    public static void main(String[] args) {
+        String nama = "Rahma";
+        int umur = 20;
+        System.out.println("Nama Saya "+nama + " Umur Saya "+umur);
+    }
+}
