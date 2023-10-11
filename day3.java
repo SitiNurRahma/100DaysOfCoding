ff00c95d24a9241d68812019d06ea5611e73094e
SitiNurRahma <sitinurrahma249@gmail.com>
2023-10-11 21:30:00 +0800

    day3

---
+++ day3.java
@@ -0,0 +1,23 @@
+
+package ISC;
+
+public class day3 {
+     public static void main(String[] args) {
+        //Deklarasi variabel menggunakan tipe data primitif
+        int umur = 20;
+        float TinggiBadan = 159.0f;
+        double BeratBadan = 63.0;
+        char JenisKelamin = 'P';
+        boolean Mahasiswa = true;
+        byte JumlahSaudara = 3;
+        short UkuranSepatu = 40;
+        
+        System.out.println("Umur saya saat ini adalah: " + umur + "Tahun");
+        System.out.println("Tinggi badan saya yaitu: " + TinggiBadan);
+        System.out.println("Berat badan saya: " + BeratBadan);
+        System.out.println("Jenis kelamin saya: " + JenisKelamin);
+        System.out.println("Saat ini saya berstatus sebagai mahasiswa di unsulbar: " + Mahasiswa);
+        System.out.println("JUmlah saudara saya ada : " + JumlahSaudara);
+        System.out.println("Ukuran sepatu saya yaitu: " + UkuranSepatu);
+    }
+}
