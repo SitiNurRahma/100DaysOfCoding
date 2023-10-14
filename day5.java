55ad29077b10c89354ab4735e3c4ea9b181d1052
SitiNurRahma <sitinurrahma249@gmail.com>
2023-10-14 23:27:49 +0800

    day5

---
+++ day5.java
@@ -0,0 +1,16 @@
+package ISC;
+
+public class day5 {
+    public static void main(String[] args) {
+        //Mengkonversi String ke tipe data primitif
+        
+        String angka = "2003";
+        //Konversi String ke tipe data integer
+        int bilangan = Integer.parseInt(angka);
+        
+        System.out.println("Hasil dari konversi variabel String ke integer ");
+        System.out.println("Bilangan: " + angka);
+        
+    }
+        
+}
