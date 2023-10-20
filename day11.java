0a5bb5e97cccd62e33c6ae439cfc6beae4bcf4fd
SitiNurRahma <sitinurrahma249@gmail.com>
2023-10-20 22:04:00 +0800

    day11

--- day11.java
+++ day11.java
@@ -1,7 +1,19 @@
 package ISC;
 
+import java.util.Scanner;
+
 public class day11 {
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan angka : ");
+        int angka = sc.nextInt();
+        
+        if(angka %2 ==0 ){
+            System.out.print("Bilangan Genap ");
+        }else{
+            System.out.print("Bilangan ganjil ");
+        }
     }
 }
