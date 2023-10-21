374c0934ebea88f726253c32de14735b43f018ec
SitiNurRahma <sitinurrahma249@gmail.com>
2023-10-22 07:06:17 +0800

    Menampilkan angka dari 20 hingga angka 200 menggunakan perulangan for

--- day13.java
+++ day13.java
@@ -2,7 +2,7 @@ package ISC;
 
 public class day13 {
     public static void main(String[] args) {
-        int angka = 20;
+        
         for (int i = 20;i <= 200 ; i++) {
             System.out.println(i);
         } 
