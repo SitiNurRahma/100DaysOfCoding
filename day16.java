2397a0c4d3891301e1d3f27cfb745b621fe76f85
SitiNurRahma <sitinurrahma249@gmail.com>
2023-10-24 23:17:47 +0800

    Menampilkan elemen dalam array menggunakan for

---
+++ day16.java
@@ -0,0 +1,12 @@
+package ISC;
+
+public class day16 {
+    public static void main(String[] args) {
+        
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Elemen ke-" + i + ": " + array[i]);
+        }
+    }
+}
