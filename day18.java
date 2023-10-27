08f32f60b4a7b3a1191421f2a254f1fe6f06683b
SitiNurRahma <sitinurrahma249@gmail.com>
2023-10-27 16:45:45 +0800

    Melakukan pengurangan dari angka 10 ke angka 1 menggunakan perulangan for

---
+++ day18.java
@@ -0,0 +1,14 @@
+package ISC;
+
+public class day18 {
+    public static void main(String[] args) {
+        
+        int total = 10;
+        for (int i = 9; i >= 1; i--) {
+            total -= i;
+        }
+            System.out.println("Hasil pengurangan : " +total);
+        
+        
+    }
+}
