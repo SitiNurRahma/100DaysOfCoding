243e5ca65899d4c1bed0873bdcde2df2a4972d1d
SitiNurRahma <sitinurrahma249@gmail.com>
2023-10-27 16:48:56 +0800

    Melakukan pengurangan dari angka 10 ke angka 1 menggunakan perulangan while

---
+++ day19.java
@@ -0,0 +1,13 @@
+package ISC;
+
+public class day19 {
+    public static void main(String[] args) {
+        int angka = 9;
+        int total = 10;
+        while(angka >= 1 ){
+            total -= angka;
+            angka--;
+            System.out.println("hasil pengurangan : " +total);
+        }
+    }
+}
