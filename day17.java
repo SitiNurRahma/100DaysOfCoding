bda5d8ca4be042028f1c532245702e5f59f1a914
SitiNurRahma <sitinurrahma249@gmail.com>
2023-10-27 15:08:20 +0800

    Menjumlahkan angka dari 1 hingga angka 10 menggunakan perulangan for

---
+++ day17.java
@@ -0,0 +1,12 @@
+package ISC;
+
+public class day17 {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 1; i <= 10; i++) {
+            total += i;
+        }
+            System.out.println("Hasil Penjumlahan : " +total); 
+    }
+}
+
