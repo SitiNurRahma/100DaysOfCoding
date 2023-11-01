0827a4898044e04eb7cd946406d112dccd2d9774
SitiNurRahma <sitinurrahma249@gmail.com>
2023-11-02 07:14:12 +0800

    Percabangan dengan Switch Case

---
+++ day21.java
@@ -0,0 +1,21 @@
+package ISC;
+
+public class day21 {
+    public static void main(String[] args) {
+         int pilihan = 2; 
+
+        switch (pilihan) {
+            case 1:
+                System.out.println("Anda memilih angka 1.");
+                break;
+            case 2:
+                System.out.println("Anda memilih angka 2.");
+                break;
+            case 3:
+                System.out.println("Anda memilih angka 3.");
+                break;
+            default:
+                System.out.println("Pilihan tidak valid.");
+        }
+    }
+}
