1d8cad966a8830b09fc744ea6217e4f6268d18e2
SitiNurRahma <sitinurrahma249@gmail.com>
2023-11-02 07:17:54 +0800

    Perulangan for-each

---
+++ day23.java
@@ -0,0 +1,11 @@
+package ISC;
+
+public class day23 {
+    public static void main(String[] args) {
+         int[] angka = {1, 2, 3, 4, 5};
+
+        for (int nilai : angka) {
+            System.out.println(nilai);
+        }
+    }
+}
