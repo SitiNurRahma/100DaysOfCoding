dbdbcfeff90480a5aba095f2d590cfa01931fdfd
SitiNurRahma <sitinurrahma249@gmail.com>
2023-11-02 07:19:26 +0800

    Perulangan Do While

---
+++ day24.java
@@ -0,0 +1,13 @@
+
+package ISC;
+
+public class day24 {
+    public static void main(String[] args) {
+        int angka = 1;
+        
+        do {
+            System.out.println("Ini adalah iterasi ke-" + angka);
+            angka++;
+        } while (angka <= 5);
+    }
+}
