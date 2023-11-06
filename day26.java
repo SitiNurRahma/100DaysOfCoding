9bcad809b11617111a52175efd760afb7292b1b4
SitiNurRahma <sitinurrahma249@gmail.com>
2023-11-06 23:26:56 +0800

    Perulangan Break

---
+++ day26.java
@@ -0,0 +1,15 @@
+
+package ISC;
+
+public class day26 {
+      public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i==3){
+                System.out.println("Menghentikan perulangan ketika i = 3");
+                break;
+            }
+            System.out.println("Nilai i:");
+            
+        }
+    }
+}
