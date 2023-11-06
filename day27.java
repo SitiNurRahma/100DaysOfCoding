2796a375f9b507abb3e583a6b35a100269b503c7
SitiNurRahma <sitinurrahma249@gmail.com>
2023-11-06 23:27:56 +0800

    Perulangan Continue

---
+++ day27.java
@@ -0,0 +1,15 @@
+package ISC;
+
+public class day27 {
+     public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i==3){
+                System.out.println("Menghentikan perulangan ketika i = 3");
+                continue;
+            }
+            System.out.println("Nilai i:");
+            
+        }
+    
+    }
+}
