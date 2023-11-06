6adac0dc56c501ac95535805ce34dbdbb91358cb
SitiNurRahma <sitinurrahma249@gmail.com>
2023-11-06 23:48:34 +0800

    Array tampilkan item menggunakan perulangan

---
+++ day30.java
@@ -0,0 +1,12 @@
+package ISC;
+
+public class day30 {
+     public static void main(String[] args) {
+        int [] angka = {1,2,3,4,5};
+        
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Item ke-" + i + ":" + angka[i]);
+            
+        }
+    }
+}
