e9079d902f4ceb8c536ea993aa5ba17c5d56c09a
SitiNurRahma <sitinurrahma249@gmail.com>
2023-11-11 22:28:56 +0800

    ArrayList

---
+++ day32.java
@@ -0,0 +1,21 @@
+
+package ISC;
+
+import java.util.ArrayList;
+
+public class day32 {
+    public static void main(String[] args) {
+
+        ArrayList<String> daftarNama = new ArrayList<>();
+
+        daftarNama.add("Rahma");
+        daftarNama.add("Reni");
+        daftarNama.add("Lisa");
+        daftarNama.add("Ica");
+
+        for (String nama : daftarNama) {
+            System.out.println("Daftar nama : " + nama);
+        }
+    }
+
+}
