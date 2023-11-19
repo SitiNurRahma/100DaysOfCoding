36641857d910ef19e0f487ba9340bdcdb1a3def0
SitiNurRahma <sitinurrahma249@gmail.com>
2023-11-19 14:12:28 +0800

    setiap inputan akan dijumlahkan terus menerus

---
+++ day40.java
@@ -0,0 +1,27 @@
+package ISC;
+
+import java.util.Scanner;
+
+public class day40 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int total = 0;
+
+        while (true) {
+            System.out.print("Masukkan angka: ");
+            int angka = scanner.nextInt();
+
+            total += angka;
+
+            System.out.println("Total Penjumlahan : " + total);
+
+            System.out.print("Ingin memasukkan angka lagi? (Y/N): ");
+            String input = scanner.next();
+
+            if (input.equals("N")) {
+                break;
+            }
+        }
+    }
+}
