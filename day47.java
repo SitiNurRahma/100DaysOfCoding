f3b9250ee78e578dcf54984c1ae1c72c90adcead
SitiNurRahma <sitinurrahma249@gmail.com>
2023-11-26 08:35:16 +0800

    inputan Array

---
+++ day47.java
@@ -0,0 +1,25 @@
+
+package ISC;
+
+import java.util.Scanner;
+
+public class day47 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan panjang array : ");
+        
+        int panjang = sc.nextInt();
+        int[] numbers = new int[panjang];
+        
+        for (int i = 0; i < panjang; i++) {
+            System.out.print("nilai : "); 
+            int nilai = sc.nextInt();
+            numbers[i] = nilai;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+            
+        }
+    }
+}
+
