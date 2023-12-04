be2d43766317500beb022e974cd0947340382c8d
SitiNurRahma <sitinurrahma249@gmail.com>
2023-12-04 12:30:38 +0800

    Menampilkan angka genap atau ganjil sesuai inputannya

---
+++ day55.java
@@ -0,0 +1,26 @@
+package ISC;
+import java.util.Scanner;
+
+public class day55 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan Batas Angka:");
+        int batas_angka = sc.nextInt();
+
+        if (batas_angka % 2 == 0) {
+            System.out.println("Angka Genap");
+            for (int i = 2; i <= batas_angka; i += 2) {
+                System.out.println(i + "");
+
+            }
+        } else {
+            System.out.println("Angka Ganjil");
+            for (int i = 1; i <= batas_angka; i += 2) {
+                System.out.println(i + "");
+            }
+        }
+        sc.close();
+    }
+}
