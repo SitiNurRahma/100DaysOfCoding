a42856493e767ec9453f3009a3a1eb1a1451ae47
SitiNurRahma <sitinurrahma249@gmail.com>
2023-12-16 21:47:08 +0800

    array

--- day69.java
+++ day69.java
@@ -2,7 +2,7 @@
 package ISC;
 public class day69 {
     public static void main(String[] args) {
-        int[] array = {1, 7, 35,95, 5};
+        int[] array = {1, 7, 35,98, 5};
         int maksimum = array[0];
 
         for (int element : array) {
