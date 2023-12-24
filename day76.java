685ae2132ccbba653278f38736729b9f4b2e5da5
SitiNurRahma <sitinurrahma249@gmail.com>
2023-12-24 20:56:11 +0800

    Array

---
+++ day76.java
@@ -0,0 +1,17 @@
+
+package ISC;
+
+public class day76 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+        // Menghitung rata-rata
+        int sum = 0;
+        for (int value : array) {
+            sum += value;
+        }
+        double average = (double) sum / array.length;
+
+        System.out.println("Rata-rata dari array adalah: " + average);
+    }
+}
