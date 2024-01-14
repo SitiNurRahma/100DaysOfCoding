ae0b4e66281a4bbd5cc63b2361a182365ed20d62
SitiNurRahma <sitinurrahma249@gmail.com>
2024-01-14 20:23:59 +0800

    Aritmetika

--- day98.java
+++ day98.java
@@ -7,13 +7,13 @@ public class day98 {
     public static void aritmetika(int angka) {
         for (int i = 1; i <= angka; i++) {
             if (i % 2 == 0 && i % 3 == 0) {
-                System.out.println(i + " adalah kelipatan 2 dan 3");
+                System.out.println(i + " Adalah kelipatan 2 dan 3");
             } else if (i % 2 == 0) {
-                System.out.println(i + " adalah kelipatan 2");
+                System.out.println(i + " Adalah kelipatan 2");
             } else if (i % 3 == 0) {
-                System.out.println(i + " adalah kelipatan 3");
+                System.out.println(i + " Adalah kelipatan 3");
             } else {
-                System.out.println(i + " bukan kelipatan 2 atau 3");
+                System.out.println(i + " Bukan kelipatan 2 atau 3");
             }
         }
     }
