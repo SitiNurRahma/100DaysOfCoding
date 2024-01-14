e91f7491bb9189d6099544f6a9cb51c9f3b0d8bb
SitiNurRahma <sitinurrahma249@gmail.com>
2024-01-14 20:20:11 +0800

    Aritmetika

---
+++ day98.java
@@ -0,0 +1,27 @@
+
+package ISC;
+
+import java.util.Scanner;
+
+public class day98 {
+    public static void aritmetika(int angka) {
+        for (int i = 1; i <= angka; i++) {
+            if (i % 2 == 0 && i % 3 == 0) {
+                System.out.println(i + " adalah kelipatan 2 dan 3");
+            } else if (i % 2 == 0) {
+                System.out.println(i + " adalah kelipatan 2");
+            } else if (i % 3 == 0) {
+                System.out.println(i + " adalah kelipatan 3");
+            } else {
+                System.out.println(i + " bukan kelipatan 2 atau 3");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int inputAngka = scanner.nextInt();
+        aritmetika(inputAngka);
+    }
+}
