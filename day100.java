4e3dcee4774b17f5797bfb4ffb4f3d7cb02c91d5
SitiNurRahma <sitinurrahma249@gmail.com>
2024-01-16 21:56:21 +0800

    Last day

---
+++ day100.java
@@ -0,0 +1,18 @@
+package ISC;
+
+import java.util.Scanner;
+
+public class day100 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Masukkan angka : ");
+        int angka = sc.nextInt();
+        System.out.println(angka);
+        
+        if(angka <100){
+            System.out.println("Maaf anda tidak lolos");
+        }else{
+            System.out.println("Selamat anda lolos");
+        }
+    }
+}
